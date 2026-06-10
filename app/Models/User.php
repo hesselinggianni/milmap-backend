476231299de0794c73bacafc7b37f2aeff10a1e0
Gianni <hesselinggianni@gmail.com>
2026-06-10 23:10:14 +0200

    Update

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,6 +56,7 @@ class User extends Authenticatable
             'trial_ends_at'     => 'datetime',
             'view_only'         => 'boolean',
             'last_seen_at'      => 'datetime',
+            'key_escrow_updated_at' => 'datetime',
         ];
     }
 
